a4ed0ca04ccc015920169e47ababce80bee1c549
IgnacioBergantinosGM2 <bergantinos.ignacio@gmail.com>
2024-02-22 18:02:27 -0300

    feat(Ultis): add parser utils for the projects

---
+++ Utils/Project utils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Newtonsoft.Json;
+using workersApi.Models;
+
+namespace workersApi.Utils
+{
+	public class ProjectUtils
+	{
+		public ProjectUtils()
+		{
+		}
+
+		public static List<Project> ParseProject(List<DBProject> projects)
+		{
+            List<Project> projectsList = new();
+
+            foreach (DBProject project in projects)
+            {
+                List<Assigned> assigned = JsonConvert.DeserializeObject<List<Assigned>>(project.Assigned);
+
+                Project parseProject = new()
+                {
+                    Id = project.Id,
+                    Name = project.Name,
+                    Assigned = assigned
+                };
+
+                projectsList.Add(parseProject);
+            }
+
+            return projectsList;
+        }
+
+        public static Project ParseProject(DBProject project)
+        {
+
+            List<Assigned> assigned = JsonConvert.DeserializeObject<List<Assigned>>(project.Assigned);
+
+            Project parseProject = new()
+            {
+                Id = project.Id,
+                Name = project.Name,
+                Assigned = assigned
+            };
+
+            return parseProject;
+        }
+
+    }
+}
+
